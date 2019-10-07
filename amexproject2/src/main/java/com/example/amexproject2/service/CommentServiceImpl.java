6c729cb10aec173bd54bddcc02c96aa16b329453
Samantha Richards <srichards567@gmail.com>
2019-10-07 09:58:53 -0400

    updated comment repo and service.

--- amexproject2/src/main/java/com/example/amexproject2/service/CommentServiceImpl.java
+++ amexproject2/src/main/java/com/example/amexproject2/service/CommentServiceImpl.java
@@ -1,13 +1,30 @@
 package com.example.amexproject2.service;
 
 import com.example.amexproject2.model.Comment;
+import com.example.amexproject2.repository.CommentRepository;
 import com.example.amexproject2.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentServiceImpl implements CommentService {
+   @Autowired
+    CommentRepository commentRepository;
+
     @Override
     public Comment createComment(Comment newComment) {
-        return
+        return commentRepository.save(newComment);
+    }
+
+    @Override
+    public HttpStatus deleteComment(Long id) {
+        commentRepository.deleteById(id);
+        return HttpStatus.OK;
+    }
+
+    @Override
+    public Iterable<Comment> listAllComments() {
+       return commentRepository.findAll();
     }
 }

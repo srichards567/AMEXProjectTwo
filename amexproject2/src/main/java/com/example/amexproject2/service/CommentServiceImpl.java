c1eddb9d6156b1fb7158af23f73ba539145fa7b7
Samantha Richards <srichards567@gmail.com>
2019-10-07 09:42:47 -0400

    comment model, service, and service impl added.

---
+++ amexproject2/src/main/java/com/example/amexproject2/service/CommentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.amexproject2.service;
+
+import com.example.amexproject2.model.Comment;
+import com.example.amexproject2.service.CommentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+    @Override
+    public Comment createComment(Comment newComment) {
+        return
+    }
+}

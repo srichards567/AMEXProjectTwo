9cb0dad5249a23d37823f7d2e7a68f44e9c59680
Samantha Richards <srichards567@gmail.com>
2019-10-09 11:40:23 -0400

    added user signup

--- front-end/js/app.js
+++ front-end/js/app.js
@@ -28,3 +28,44 @@ function loginUser() {
             console.log(error);
         })
 }
+
+function userSignup() {
+
+  const userEmail = document.querySelector('.signupEmail').value;
+  const userName = document.querySelector('.signupUsername').value;
+  const userPassword = document.querySelector('.signupPassword').value;
+  const confirmPassword = document.querySelector('.reEnterPassword').value;
+  const signupPassword;
+  if (userPassword == confirmPassword) {
+    signupPassword = confirmPassword;
+  } else {
+    alert("passwords do not match. please re-enter.")
+  }
+
+
+
+  fetch('http://localhost:8181/signup',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: userEmail,
+        username: userName,
+        password: signupPassword
+      })
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      localStorage.setItem('user', res.token);
+      if (res.token) { // DO I GET A RESPONSE? IF YES:
+          console.log(localStorage.getItem('user'));
+          window.location.href = "index.html";
+      }
+  })
+  .catch((error) => {
+      console.log(error);
+  })
+}

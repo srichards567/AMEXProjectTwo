985c41d1a99be1d5fd81e2f20db103cad7ab559a
Christine Le <christine.le244@gmail.com>
2019-10-10 13:53:41 -0400

    users can see and make a post

--- front-end/js/home.js
+++ front-end/js/home.js
@@ -1,4 +1,5 @@
-//to list all posts
+// =========== SEE ALL POSTS ==================
+//
 const getAllPosts = function() {
   const postsContainer = document.querySelector('.posts');
   fetch('http://localhost:8181/post/list/all', {
@@ -11,9 +12,17 @@ const getAllPosts = function() {
     return response.json();
   })
   .then((response) => {
+    postsContainer.innerHTML = "";
     for(let i = 0; i < response.length; i++) {
       const newPost = document.createElement('div');
       newPost.classList = 'allPosts';
+
+      const deleteBtn = document.createElement('i');
+      deleteBtn.setAttribute("postid", response[i].id);
+      deleteBtn.classList = "fa fa-times";
+      newPost.append(deleteBtn);
+
+
       const title = document.createElement('h2');
       title.innerText = response[i].title;
       newPost.appendChild(title);
@@ -27,43 +36,27 @@ const getAllPosts = function() {
     console.log(err);
   })
 }
-        //         const list = document.querySelector('.allPosts')
 
-        //           const comment = document.createElement('input');
-        //           const addComment = document.createElement('button');
-        //           const deleteComment = document.createElement('button');
-        //           const deletePost = document.createElement('button');
-        //
+// =========== DELETE A POST ==================
 
-        //           document.querySelector('.allPosts').appendChild(comment);
-        //           document.querySelector('.allPost').appendChild(addComment);
-        //           document.querySelector('.allPosts').appendChild(deleteComment);
-        //           document.querySelector('.allPosts').appendChild(deletePost);
-        //
-        //           addComment.setAttribute("type", "submit");
-        //           addComment.setAttribute("value", "add");
-        //           deleteComment.setAttribute("type", "submit");
-        //           deleteComment.setAttribute("value", "delete");
-
-
-
-const getData = function(data) {
-  const title = data.title;
-  const body = data.body;
+function requestDeletePost(id) {
+  fetch(`http://localhost:8181/post/${id}`, {
+      method: 'DELETE',
+      headers: {
+            "Authorization": "Bearer " + localStorage.getItem('user'),
+            "Content-Type": "application/json"
+          }
+        })
+    .then((res) => {
+      getAllPosts()
+    })
+    .catch((error) => {
+    console.log(id);
+    })
 }
 
-function updateDOM(data) {
-  const title = document.querySelector('.title');
-  const body = document.querySelector('.body');
-  const comment = document.querySelector('.comment');
+// =========== DELETE A COMMENT ==================
 
-  title.innerText = `Title: ${data.title}`;
-  body.innerText = `Body: ${data.body}`;
-  comment.innerText = `Comment: ${data.comment}`;
-}
-
-
-//to delete comments
 function deleteComment() {
   fetch(`http://localhost:8181/comment/{comment_id}`, {
     method: 'DELETE',
@@ -84,7 +77,8 @@ function deleteComment() {
     })
 }
 
-//to list user comments
+// =========== LIST MY COMMENTS ==================
+
 const makeCall1 = function() {
 fetch(`http://localhost:8181/comment/list`)
   .then((response) => {
@@ -106,70 +100,57 @@ fetch(`http://localhost:8181/comment/list`)
 const getData1 = function(data) {
   const body = data.commentBody;
 }
-// const makeCall = function() {
-// fetch(`http://localhost:8181/comment/list`)
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((response) => {
-//     const list = document.querySelector('.userComments')
-//     for (let i = 0; i < response.length; i ++) {
-//       const body = document.createElement('p');
-//
-//       document.querySelector('.userComments').appendChild(body);
-//     }
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   })
-// }
-//
-// const getData = function(data) {
-//   const body = data.commentBody;
-// }
 
 // =========== DOM MANIPULATION ==================
-//
-// function showMakeProfile() {
-//   document.querySelector('.makeProfile').style.display="inline";
-// }
-//
-// function hideMakeProfile() {
-//   document.querySelector('.makeProfile').style.display="none";
-//   showViewProfileBtn();
-//   hideCreateProfileBtn();
-// }
-//
-// function showViewProfileBtn() {
-//   document.querySelector('.viewProfileBtn').style.display="inline";
-// }
-//
-// function hideCreateProfileBtn() {
-//   document.querySelector('.createProfileBtn').style.display="none";
-// }
-//
-// function showViewerProfile() {
-//   document.querySelector('.userProfile').style.display = "block";
-//   showUpdateProfileBtn();
-// }
-//
-// function hideUserProfile() {
-//   document.querySelector('.userProfile').style.display = "none";
-//   document.querySelector('.updateUserProfile').style.display="none";
-// }
-//
-// function showUpdateUserProfile() {
-//   document.querySelector('.updateUserProfile').style.display="block";
-// }
-//
-// function hideUpdateProfileBtn() {
-//   document.querySelector('.updateProfileBtn').style.display="none";
-// }
-//
-// function showUpdateProfileBtn() {
-//   document.querySelector('.updateProfileBtn').style.display="inline";
-// }
 
+function showMakeProfile() {
+  document.querySelector('.makeProfile').style.display="inline";
+}
+
+function hideMakeProfile() {
+  document.querySelector('.makeProfile').style.display="none";
+  showViewProfileBtn();
+  hideCreateProfileBtn();
+}
+
+function showViewProfileBtn() {
+  document.querySelector('.viewProfileBtn').style.display="inline";
+}
+
+function hideCreateProfileBtn() {
+  document.querySelector('.createProfileBtn').style.display="none";
+}
+
+function showViewerProfile() {
+  document.querySelector('.userProfile').style.display = "block";
+  showUpdateProfileBtn();
+}
+
+function hideUserProfile() {
+  document.querySelector('.userProfile').style.display = "none";
+  document.querySelector('.updateUserProfile').style.display="none";
+}
+
+function showUpdateUserProfile() {
+  document.querySelector('.updateUserProfile').style.display="block";
+}
+
+function hideUpdateProfileBtn() {
+  document.querySelector('.updateProfileBtn').style.display="none";
+}
+
+function showUpdateProfileBtn() {
+  document.querySelector('.updateProfileBtn').style.display="inline";
+}
+
+function showUserProfile() {
+  document.querySelector('.userProfile').style.display="inline";
+}
+
+function eraseText() {
+  document.querySelector('.makePostTitle').value = "";
+  document.querySelector('.makePostBody').value = "";
+}
 // =========== MANIPULATE DOM WITH PROMISE VALUES ==================
 function manipulateDom(htmlElementId, res) {
   const targetElement = document.getElementById(htmlElementId);
@@ -235,7 +216,30 @@ function getUserProfile() {
 }
 
 
+// =========== MAKE A POST ==================
+function makeUserPost() {
+  const postTitle = document.querySelector('.makePostTitle').value;
+  const postBody = document.querySelector('.makePostBody').value;
 
+  fetch('http://localhost:8181/post', {
+    method: 'POST',
+    headers: {
+      'Authorization': 'Bearer ' + localStorage.getItem('user'),
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: postTitle,
+      body: postBody
+    })
+  })
+  .then((res) => {
+    alert('Your post was successfuly made!');
+    getAllPosts();
+  })
+  .then((err) => {
+    console.log(err);
+  })
+}
 // =========== UPDATE A PROFILE ==================
 // =========== SEE USER POSTS ====================
 // =========== SEE USER COMMENTS =================
